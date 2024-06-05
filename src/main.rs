f61dd6cf14fbcad4b2fbe73417c309734c87244a
Jacob Lauxman <jlauxman@gmail.com>
2024-06-04 17:43:40 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,65 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
+    let stdin = io::stdin();
     loop {
-        let stdin = io::stdin();
         let mut input = String::new();
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        // Wait for user input
         let _ = stdin.read_line(&mut input).unwrap();
+        let cmd = input.trim();
 
-        match input.trim() {
-            "exit 0" => std::process::exit(0),
-            _ => println!("{}: command not found", input.trim()),
+        if cmd.is_empty() {
+            continue;
+        }
+
+        match parse_command(&input) {
+            Some(ShellCommand::Exit(n)) => std::process::exit(n),
+            Some(ShellCommand::Echo(echo)) => print!("{echo}"),
+            Some(ShellCommand::Type(cmd)) => command_type(cmd),
+            _ => print!("{}: command not found\n", input.trim()),
         };
     }
+}
+
+fn parse_command(input: &str) -> Option<ShellCommand> {
+    let (cmd, args) = match input.split_once(' ') {
+        Some((cmd, args)) => (cmd, args),
+        None => (input, ""),
+    };
+
+    match cmd {
+        "exit" => Some(ShellCommand::Exit(args.trim().parse::<i32>().unwrap())),
+        "echo" => Some(ShellCommand::Echo(args)),
+        "type" => Some(ShellCommand::Type(args)),
+        _ => None,
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum ShellCommand<'a> {
+    Exit(i32), // process::exit expects an i32 value
+    Echo(&'a str),
+    Type(&'a str),
+}
+
+#[derive(Debug, Clone)]
+pub enum CommandType {
+    BuiltIn,
+    Unrecognized,
+}
+
+fn command_type(cmd: &str) {
+    let cmd_type = match parse_command(cmd) {
+        Some(ShellCommand::Echo(_)) => CommandType::BuiltIn,
+        Some(ShellCommand::Exit(_)) => CommandType::BuiltIn,
+        Some(ShellCommand::Type(_)) => CommandType::BuiltIn,
+        _ => CommandType::Unrecognized,
+    };
+
+    match cmd_type {
+        CommandType::BuiltIn => print!("{} is a shell builtin", cmd),
+        CommandType::Unrecognized => print!("{} not found", cmd),
+    };
 }
